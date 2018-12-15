68f33d51e6d58d63749ec07b9905b613d6455ef6
M.Ahmed Arshed <31464251+AhmedArshed@users.noreply.github.com>
2018-12-15 16:06:51 +0500

    Update Dubli_Ring.cpp

--- Dubli_Ring.cpp
+++ Dubli_Ring.cpp
@@ -8,68 +8,126 @@ struct node{
 };
 
 class dub_ring{
-	private:
-		node *head , *temp , *ptr;
-		int key;
-	public:
-		dub_ring()
+private:
+	node *head , *ptr , *temp;
+	int key;
+public:
+	dub_ring()
+	{
+		head = temp = ptr = NULL;
+		key = 0;
+	}
+	~dub_ring()
+	{
+		delete head;
+		delete temp;
+	}
+	void insert()
+	{
+		if(head == NULL)
 		{
-			head = temp = ptr = NULL;
-			key = 0;
+			cout <<"Enter value "<<endl;
+			head = new node;
+			cin >> head->info;
+			head->next = head;
+			head->previous = head;
+			return;
 		}
-		~dub_ring()
+		if(head->next == head)
 		{
-			delete temp;
-			delete head;
-			delete ptr;
+			cout << "Enter value" <<endl;
+			temp = new node;
+			cin >> temp->info;
+			temp->next = head;
+			temp->previous = head;
+			head->next = temp;
+			head->previous = temp;
+			return;
 		}
-		void start()
-		{
-			if (head == NULL)
-			{
-				head = new node;
-				cout << "Enter info for head "<<endl;
-				cin >> head -> info;
-				head -> next = head;
-				head -> previous = head;
-				return;
-			}
-			if (head->next == head)
-			{
-				temp = new node;
-				cout << "Enter value "<<endl;
-				cin >> temp -> info;
-				temp -> next = head;
-				head -> next = temp;
-				head -> previous = temp;
-				temp -> previous = head;
-				return;
-			}
-			temp = ptr;
-			ptr = new node;
-			cout << "Enter value "<<endl;
-			cin >> ptr->info;
-			temp -> next = ptr;
-			ptr -> next = head;
-			ptr -> previous = temp;
-			head -> previous = ptr;
-		}
+		while(temp->next != head)
+			temp = temp->next;
+		ptr = temp;
+		temp = new node;
+		cout << "Enter value" <<endl;
+		cin >> temp->info;
+		temp->next = head;
+		temp->previous = ptr;
+		ptr->next = temp;
+		return;
+	}
 	void print()
 	{
-		temp = head -> next;
-		while(temp !=head)
+		temp = head;
+		cout << temp->info <<" ";
+		temp = head->next;
+		while(temp != head)
 		{
-			cout << temp -> info <<" ";
-			temp = temp -> next;
+			cout << temp->info << " ";
+			temp = temp->next;		
 		}
-		cout << temp -> info <<endl;
+		cout << endl;
+	}	
+void insert_mid()
+	{
+		cout << "Enter key to insert value "<<endl;
+		cin >> key;
+			do{
+				if(temp->info == key){
+					ptr = temp;
+					temp = new node;
+					cout << "Enter value "<<endl;
+					cin >> temp->info;
+					temp->next = ptr->next;
+					temp->previous = ptr;
+					temp->next->previous = temp;
+					ptr->next = temp;
+					
+				}
+				temp = temp->next;
+			}
+			while(temp != head);
 	}
+	void delet(){
+			cout << "Enter the Delete Number " << endl;
+			int key;
+			cin >> key;
+			temp = head;
+			if(head == NULL)
+				cout << "No value in Link list" << endl;
+			else{
+				do{
+					if(key == temp->next->info && temp->next->info== head->info){
+						ptr = head;
+						head = head->next;
+						temp->next = head;
+						head->previous = temp;
+						delete ptr;
+						ptr = NULL;
+						return;
+					}
+					else if(temp->next->info == key){
+						ptr = temp->next;
+						temp->next = ptr->next;
+						ptr->next->previous = temp;
+						delete ptr;
+						ptr = NULL;
+						return;
+					}
+					temp = temp->next;
+				}
+				while(temp != head);
+			}
+		}
 };
+
 int main()
 {
-	dub_ring r;
-	r.start();
-	r.start();
-	r.start();
-	r.print();
+	dub_ring d;
+	for(int i = 0 ; i < 9 ; i++)
+		d.insert();
+	d.print();
+	d.insert_mid();
+	d.print();
+	d.delet();
+	d.print();
 }
